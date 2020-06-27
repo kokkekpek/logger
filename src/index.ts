d3febe72f6d323ba1c2bbf96a8623e95cf3b6b2c
Kok Kek <kokkekpek@gmail.com>
2020-06-28 01:18:08 +0300

    Export interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import Logger from './logger/Logger'
 import EmptyLogger from './logger/EmptyLogger'
 import LoggerInterface from './logger/LoggerInterface'
+import ConfigInterface from './logger/config/ConfigInterface'
+import ObjectInterface from './logger/data/ObjectInterface'
 export {Logger as default}
-export {EmptyLogger, LoggerInterface}
+export {EmptyLogger, LoggerInterface, ConfigInterface, ObjectInterface}
