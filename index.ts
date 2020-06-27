2616588914336ecca7debff7b6f6c88ccb60ba77
Kok Kek <kokkekpek@gmail.com>
2020-06-27 18:28:48 +0300

    Fix imports in index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import Logger from './src/Logger'
-import EmptyLogger from './src/Logger'
+import EmptyLogger from './src/EmptyLogger'
 import LoggerInterface from './src/LoggerInterface'
 export {Logger as default}
 export {EmptyLogger, LoggerInterface}
