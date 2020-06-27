c9f24c866322a21f51ae307c898007c83a8cfc49
Kok Kek <kokkekpek@gmail.com>
2020-06-27 22:49:42 +0300

    Move index.ts to src/

--- index.ts
+++
@@ -1,5 +0,0 @@
-import Logger from './src/Logger'
-import EmptyLogger from './src/EmptyLogger'
-import LoggerInterface from './src/LoggerInterface'
-export {Logger as default}
-export {EmptyLogger, LoggerInterface}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import Logger from './logger/Logger'
+import EmptyLogger from './logger/EmptyLogger'
+import LoggerInterface from './logger/LoggerInterface'
+export {Logger as default}
+export {EmptyLogger, LoggerInterface}
